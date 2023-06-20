9596e6e83de319efabbfeec217251bf1ad0ad704
Rajashree Datar <rajashree.datar@gmail.com>
2023-06-20 17:21:34 -0400

    Task 6

--- final_project/index.js
+++ final_project/index.js
@@ -4,6 +4,20 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+
+let users = []
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+
 const app = express();
 
 app.use(express.json());
@@ -32,7 +46,27 @@ const username = req.body.username;
     return res.status(208).json({message: "Invalid Login. Check username and password"});
   } 
 });
- 
+
+app.post("/register", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    if (!username) {
+        return res.status(404).json({message: "Username not provided. Unable to register user."});
+    }
+    else if (!password) {
+        return res.status(404).json({message: "Password not provided. Unable to register user."});    
+    } 
+  });
+  
 const PORT =5000;
 
 app.use("/customer", customer_routes);
